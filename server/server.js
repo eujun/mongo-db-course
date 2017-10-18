716163ae4af7e41c46756f2d758670567c6d836a
Eujun Chin <eujun32@gmail.com>
2017-10-18 17:10:57 -0500

    Added PATCH/todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
-var {ObjectID} = require('mongodb');
+
 
 var app = express();
 const port = process.env.PORT || 3000
@@ -53,6 +55,7 @@ app.get('/todos/:id', (req,res) => {
   }).catch((e) => res.status(400).send());
 });
 
+//DELETE routes
 app.delete('/todos/:id', (req,res) =>{
   var id = req.params.id;
 
@@ -70,6 +73,32 @@ app.delete('/todos/:id', (req,res) =>{
   }).catch((e) => res.status(400).send());
 });
 
+//PATCH routes
+app.patch('/todos/:id', (req,res) =>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send("Invalid ID");
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send("ID not found.")
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 // Starts server
 app.listen(port, () => {
