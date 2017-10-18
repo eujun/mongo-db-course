0b09b0b1f04e9ee97a05d4d86dc164303896954d
Eujun Chin <eujun32@gmail.com>
2017-10-18 15:39:46 -0500

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -26,13 +27,33 @@ app.post('/todos', (req, res) => {
 // GET routes
 app.get('/todos', (req,res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({
+      todos
+    });
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  console.log(id);
 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send("Invalid ID");
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send("ID not found.");
+    }
+    res.send({
+      todo             // same as todo: todo
+    });
+  }).catch((e) => res.status(400).send());
+});
+
+// Starts server
 app.listen(3000, () => {
   console.log('Stated on port 3000');
 });
